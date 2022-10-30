755b22c8cca4def9564252b35953063029911d25
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-10-30 17:22:20 +0700

    Add Payments & Bidding

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import {
 } from '@thirdweb-dev/react';
 import { ListingType } from '@thirdweb-dev/sdk';
 import Header from '../components/Header';
+import Link from 'next/link';
 
 const Home = () => {
   const { contract } = useContract(
@@ -26,52 +27,55 @@ const Home = () => {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
             {listings?.map((listing) => (
-              <div
+              <Link
+                href={`/listing/${listing.id}`}
                 key={listing.id}
                 className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out"
               >
-                <div className="flex flex-1 flex-col pb-2 items-center">
-                  <MediaRenderer
-                    src={listing.asset.image}
-                    className="w-full md:w-64"
-                  />
-                </div>
-                <div className="pt-2 space-y-4">
-                  <div>
-                    <h2 className="text-2xl truncate font-medium mb-2">
-                      {listing.asset.name}
-                    </h2>
-                    <hr />
-                    <p className="truncate text-sm text-gray-600 mt-2">
-                      {listing.asset.description}
-                    </p>
+                <div>
+                  <div className="flex flex-1 flex-col pb-2 items-center">
+                    <MediaRenderer
+                      src={listing.asset.image}
+                      className="w-full md:w-64"
+                    />
                   </div>
-                  <p>
-                    <span className="font-bold mr-1">
-                      {listing.buyoutCurrencyValuePerToken.displayValue}
-                    </span>
-                    {listing.buyoutCurrencyValuePerToken.symbol}
-                  </p>
-                  <div
-                    className={`flex items-center space-x-1 justify-end text-sm border w-fit ml-auto p-2 px-4 rounded-lg text-white transition-all duration-150 ease-out ${
-                      listing.type === ListingType.Direct
-                        ? 'bg-blue-500 hover:bg-blue-900'
-                        : 'bg-red-500 hover:bg-red-900'
-                    }`}
-                  >
+                  <div className="pt-2 space-y-4">
+                    <div>
+                      <h2 className="text-2xl truncate font-medium mb-2">
+                        {listing.asset.name}
+                      </h2>
+                      <hr />
+                      <p className="truncate text-sm text-gray-600 mt-2">
+                        {listing.asset.description}
+                      </p>
+                    </div>
                     <p>
-                      {listing.type === ListingType.Direct
-                        ? 'Buy Now'
-                        : 'Auction'}
+                      <span className="font-bold mr-1">
+                        {listing.buyoutCurrencyValuePerToken.displayValue}
+                      </span>
+                      {listing.buyoutCurrencyValuePerToken.symbol}
                     </p>
-                    {listing.type === ListingType.Direct ? (
-                      <BanknotesIcon className="h-4" />
-                    ) : (
-                      <ClockIcon className="h-4" />
-                    )}
+                    <div
+                      className={`flex items-center space-x-1 justify-end text-sm border w-fit ml-auto p-2 px-4 rounded-lg text-white transition-all duration-150 ease-out ${
+                        listing.type === ListingType.Direct
+                          ? 'bg-blue-500 hover:bg-blue-900'
+                          : 'bg-red-500 hover:bg-red-900'
+                      }`}
+                    >
+                      <p>
+                        {listing.type === ListingType.Direct
+                          ? 'Buy Now'
+                          : 'Auction'}
+                      </p>
+                      {listing.type === ListingType.Direct ? (
+                        <BanknotesIcon className="h-4" />
+                      ) : (
+                        <ClockIcon className="h-4" />
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         )}
---
+++ pages/listing/[listingId].tsx
@@ -0,0 +1,264 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Header from '../../components/Header';
+import {
+  useContract,
+  useNetwork,
+  useNetworkMismatch,
+  useMakeBid,
+  useOffers,
+  useMakeOffer,
+  useBuyNow,
+  MediaRenderer,
+  useAddress,
+  useListing,
+} from '@thirdweb-dev/react';
+import { UserCircleIcon } from '@heroicons/react/24/solid';
+import { ListingType } from '@thirdweb-dev/sdk';
+import Countdown from 'react-countdown';
+import network from '../../utils/network';
+import { ethers } from 'ethers';
+
+type Props = {};
+
+function ListingPage({}: Props) {
+  const router = useRouter();
+  const { listingId } = router.query as { listingId: string };
+  const [bidAmount, setBidAmount] = useState<string>('');
+  const [, switchNetwork] = useNetwork();
+  const networkMismatch = useNetworkMismatch();
+  const [minimumNextBid, setMinimumNextBid] = useState<{
+    displayValue: string;
+    symbol: string;
+  }>();
+
+  const { contract } = useContract(
+    process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
+    'marketplace'
+  );
+
+  const { mutate: makeBid } = useMakeBid(contract);
+  const { data: offers } = useOffers(contract, listingId);
+  const { mutate: buyNow } = useBuyNow(contract);
+  const { mutate: makeOffer } = useMakeOffer(contract);
+
+  const { data: listing, isLoading, error } = useListing(contract, listingId);
+
+  console.log(offers);
+
+  useEffect(() => {
+    if (!listingId || !contract || !listing) return;
+
+    if (listing.type === ListingType.Auction) {
+      fetchMinNextBid();
+    }
+  }, [listingId, listing, contract]);
+
+  const fetchMinNextBid = async () => {
+    if (!listingId || !contract) return;
+
+    const { displayValue, symbol } = await contract.auction.getMinimumNextBid(
+      listingId
+    );
+
+    setMinimumNextBid({
+      displayValue: displayValue,
+      symbol: symbol,
+    });
+  };
+
+  const formatPlaceholder = () => {
+    if (!listing) return;
+
+    if (listing.type === ListingType.Direct) {
+      return 'Enter Offer Amount';
+    }
+    if (listing.type === ListingType.Auction) {
+      return Number(minimumNextBid?.displayValue) === 0
+        ? 'Enter Bid Amount'
+        : `${minimumNextBid?.displayValue} ${minimumNextBid?.symbol} or more`;
+    }
+  };
+
+  const buyNft = async () => {
+    if (networkMismatch) {
+      switchNetwork && switchNetwork(network);
+      return;
+    }
+
+    if (!listingId || !contract || !listing) return;
+
+    await buyNow(
+      {
+        id: listingId,
+        buyAmount: 1,
+        type: listing.type,
+      },
+      {
+        onSuccess(data, variables, context) {
+          console.log('SUCCESS: ', data, variables, context);
+          router.replace('/');
+        },
+        onError(data, variables, context) {
+          alert('ERROR: NFT could not be bought');
+          console.log('ERROR: ', data, variables, context);
+        },
+      }
+    );
+  };
+
+  const createBidOrOffer = async () => {
+    try {
+      if (networkMismatch) {
+        switchNetwork && switchNetwork(network);
+        return;
+      }
+
+      // Direct Listing
+      if (listing?.type === ListingType.Direct) {
+        if (
+          listing.buyoutPrice.toString() ===
+          ethers.utils.parseEther(bidAmount).toString()
+        ) {
+          console.log('Buyout Price met, buying NFT...');
+          buyNft();
+          return;
+        }
+        console.log('Buyout price not met, making offer...');
+        await makeOffer(
+          {
+            quantity: 1,
+            listingId,
+            pricePerToken: bidAmount,
+          },
+          {
+            onSuccess(data, variables, context) {
+              alert('Offer made successfully');
+              console.log('SUCCESS: ', data, variables, context);
+              setBidAmount('');
+            },
+            onError(data, variables, context) {
+              alert('ERROR: Offer could not be made');
+              console.log('ERROR: ', data, variables, context);
+            },
+          }
+        );
+      }
+      // Auction Listing
+      if (listing?.type === ListingType.Auction) {
+        console.log('Making bid...');
+
+        await makeBid(
+          {
+            listingId,
+            bid: bidAmount,
+          },
+          {
+            onSuccess(data, variables, context) {
+              alert('Bid made successfully');
+              console.log('SUCCESS: ', data, variables, context);
+              setBidAmount('');
+            },
+            onError(data, variables, context) {
+              alert('ERROR: Bid could not be made');
+              console.log('ERROR: ', data, variables, context);
+            },
+          }
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+        <Header />
+        <div className="text-center animate-pulse text-blue-500">
+          <p>Loading Item...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!listing) {
+    return <div>Listing not found</div>;
+  }
+
+  return (
+    <div>
+      <Header />
+      <main className="max-w-6xl mx-auto p-2 flex flex-col lg:flex-row items-center justify-center md:items-start space-y-10 space-x-12 pr-10">
+        <div className="p-10 border mx-auto lg:mx-0 w-full ml-4 md:max-w-lg">
+          <MediaRenderer src={listing.asset.image} className="w-full" />
+        </div>
+
+        <section className="md:!mt-0 pt-4 md:pt-0 !ml-4 md:ml-8 flex-1 space-y-5 pb-20 lg:pb-0">
+          <div>
+            <h1 className="text-2xl font-bold">{listing.asset.name}</h1>
+            <p className="flex items-center space-x-2 mt-3 text-xs sm:text-base border rounded-full px-4 py-2 cursor-pointer w-fit ">
+              <UserCircleIcon className="h-5" />
+              <span className="font-bold">Seller: {listing.sellerAddress}</span>
+            </p>
+            <p className="text-gray-600">{listing.asset.description}</p>
+          </div>
+          <div className="grid grid-cols-2 items-center py-2">
+            <p className="font-bold">Listing Type:</p>
+            <p>
+              {listing.type === ListingType.Direct
+                ? 'Direct Listing'
+                : 'Auction Listing'}
+            </p>
+            <p className="font-bold">Buy it now Price:</p>
+            <p className="text-4xl font-bold">
+              {listing.buyoutCurrencyValuePerToken.displayValue}{' '}
+              {listing.buyoutCurrencyValuePerToken.symbol}
+            </p>
+            <button
+              onClick={buyNft}
+              className="bg-blue-600 font-bold col-start-2 mt-4 text-white rounded-full py-4 px-10 w-56 hover:drop-shadow-xl cursor-pointer transition-all duration-500"
+            >
+              Buy Now
+            </button>
+          </div>
+          <div className="grid grid-cols-2 space-y-2 items-center justify-end">
+            <hr className="col-span-2" />
+            <p className="col-span-2 font-bold">
+              {listing.type === ListingType.Direct
+                ? 'Make an Offer'
+                : 'Bid on this Auction'}
+            </p>
+            {listing.type === ListingType.Auction && (
+              <>
+                <p>Current Minimum Bid:</p>
+                <p className="font-bold">
+                  {minimumNextBid?.displayValue} {minimumNextBid?.symbol}
+                </p>
+
+                <p>Time Remaining:</p>
+                <Countdown
+                  date={Number(listing.endTimeInEpochSeconds.toString()) * 1000}
+                />
+              </>
+            )}
+            <input
+              type="text"
+              placeholder={formatPlaceholder()}
+              onChange={(e) => setBidAmount(e.target.value)}
+              className="border p-2 rounded-lg mr-5"
+            />
+            <button
+              onClick={createBidOrOffer}
+              className="bg-red-600 font-bold text-white rounded-full py-4 px-10 w-56 hover:drop-shadow-xl cursor-pointer transition-all duration-500"
+            >
+              {listing.type === ListingType.Direct ? 'Offer' : 'Bid'}
+            </button>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default ListingPage;
